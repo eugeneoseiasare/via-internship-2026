ba7792ce6e6d99231f30620f33d4b22c31572bf2
eugeneoseiasare <eugeneosei199@gmail.com>
2026-09-25 02:17:12 +0000

    Update Task1_file_handling.sh

--- Bash_prog /Task1_file_handling.sh
+++ Bash_prog /Task1_file_handling.sh
@@ -1,1 +1,12 @@
 
+#!/bin/bash
+# Task1: File Handling
+echo "=== Task1 File Handling ==="
+FILE="students.txt"
+echo -e "Name,Age\nEugene,25\nAma,22" > $FILE
+cat $FILE
+echo "Appending Kofi..." 
+echo "Kofi,24" >> $FILE
+wc -l $FILE
+grep "Eugene" $FILE
+echo "Task1 Done"
