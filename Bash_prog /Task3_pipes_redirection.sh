99e5d33569432ddd322e1cf6483718efefdd3b33
eugeneoseiasare <eugeneosei199@gmail.com>
2026-09-25 02:18:58 +0000

    Update Task3_pipes_redirection.sh

--- Bash_prog /Task3_pipes_redirection.sh
+++ Bash_prog /Task3_pipes_redirection.sh
@@ -1,1 +1,10 @@
-
+#!/bin/bash
+# Task3: Pipes and Redirection
+echo "=== Task3 Pipes ==="
+echo "Listing files with pipes:"
+ls -l | grep ".sh"
+echo "Saving output to log:"
+ls > output.log
+cat output.log
+echo -e "apple\nbanana\napple" | sort | uniq
+echo "Task3 Done"
