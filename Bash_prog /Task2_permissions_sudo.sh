50cb582ca802fb88f5eab20eaf6045c71ee5da17
eugeneoseiasare <eugeneosei199@gmail.com>
2026-09-25 02:18:14 +0000

    Update Task2_permissions_sudo.sh

--- Bash_prog /Task2_permissions_sudo.sh
+++ Bash_prog /Task2_permissions_sudo.sh
@@ -1,1 +1,11 @@
-
+#!/bin/bash
+# Task2: Permissions and sudo
+echo "=== Task2 Permissions ==="
+touch myfile.txt
+ls -l myfile.txt
+chmod 755 myfile.txt
+ls -l myfile.txt
+echo "Checking sudo access:"
+sudo -v
+echo "Current user: $(whoami)"
+echo "Task2 Done"
